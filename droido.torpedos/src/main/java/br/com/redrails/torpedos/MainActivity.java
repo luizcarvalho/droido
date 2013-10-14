7f7d415be4ff498d160bb9d0730d3b992859a2ec
Luiz Carvalho <maximusmano@gmail.com>
2013-10-14 18:28:42 -0300

    ActionBAR OK

--- droido.torpedos/src/main/java/br/com/redrails/torpedos/MainActivity.java
+++ droido.torpedos/src/main/java/br/com/redrails/torpedos/MainActivity.java
@@ -2,9 +2,14 @@ package br.com.redrails.torpedos;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.view.MenuItemCompat;
 import android.view.Menu;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends ActionBarActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,21 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
         return true;
     }
-    
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+
+            case R.id.action_search:
+                return true;
+
+        }
+
+        return false;
+    }
+
 }
