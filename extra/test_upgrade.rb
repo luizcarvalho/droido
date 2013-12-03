9edcfb46b83c333af6a8ac62ee91957fb94b1c21
Luiz Carvalho <maximusmano@gmail.com>
2013-12-03 11:26:40 -0300

    Test Upgrade

---
+++ extra/test_upgrade.rb
@@ -0,0 +1,36 @@
+def patch(version)
+    patch_file = File.new("proxima_versao.patch","w+")
+patch_file.puts <<-eos
+diff --git droido.torpedos/src/main/AndroidManifest.xml droido.torpedos/src/main/AndroidManifest.xml
+index 53ff11c..48496c0 100644
+--- droido.torpedos/src/main/AndroidManifest.xml
++++ droido.torpedos/src/main/AndroidManifest.xml
+@@ -1,7 +1,7 @@
+ <?xml version="1.0" encoding="utf-8"?>
+ <manifest xmlns:android="http://schemas.android.com/apk/res/android"
+     package="br.com.redrails.torpedos"
+-    android:versionCode="#{version-1}"
++    android:versionCode="#{version}"
+     android:versionName="@string/version_name" >
+ 
+     <uses-permission android:name="om.android.launcher.permission.INSTALL_SHORTCUT" />
+diff --git droido.torpedos/src/main/java/br/com/redrails/torpedos/DataBaseHelper.java droido.torpedos/src/main/java/br/com/redrails/torpedos/DataBaseHelper.java
+index e9b5411..e4f0116 100644
+--- droido.torpedos/src/main/java/br/com/redrails/torpedos/DataBaseHelper.java
++++ droido.torpedos/src/main/java/br/com/redrails/torpedos/DataBaseHelper.java
+@@ -26,7 +26,7 @@ public class DataBaseHelper extends SQLiteOpenHelper{
+ 
+     private static String DB_NAME = "database.sqlite";
+     public static String TEMP_DB_NAME = "database_temp.sqlite";
+-    private static int DB_VERSION=#{version-1};//change to version of code
++    private static int DB_VERSION=#{version};//change to version of code
+     public static boolean upgrading = false;
+eos
+patch_file.close
+end
+
+#patch(22)
+#system "patch -p0 < proxima_versao.patch"
+system "./gradlew build"
+system "adb install -r droido.torpedos/build/apk/droido.torpedos-release.apk"
+system "adb shell am start -n br.com.redrails.torpedos/br.com.redrails.torpedos.LoadScreenActivity -a android.intent.action.MAIN -c android.intent.category.LAUNCHER"
