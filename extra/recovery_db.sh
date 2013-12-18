fd2356dd3fda1b5b66ce6bd5511ac706f1bd69ec
Luiz Carvalho <maximusmano@gmail.com>
2013-12-18 18:01:06 -0300

    finalizando banco de dados

--- extra/recovery_db.sh
+++ extra/recovery_db.sh
@@ -1,1 +1,1 @@
-sqlite3 ../droido.torpedos/src/main/assets/database.sqlite .dump | sqlite3 ../droido.torpedos/src/main/assets/recovered.sqlite
+sqlite3 droido.torpedos/src/main/assets/database.sqlite .dump | sqlite3 droido.torpedos/src/main/assets/recovered.sqlite
